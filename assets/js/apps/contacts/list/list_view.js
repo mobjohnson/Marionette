9ae48410b307e2563e8e245b1c1c63cf222ffd09
mobjohnson <mobjohnson@gmail.com>
2015-03-21 19:43:07 -0400

    Add delete button

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,22 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: "tr",
-    template: "#contact-list-item"
+    template: "#contact-list-item",
+
+    events: {
+     "click": "hightlightName",
+     "click button": "deleteClicked"
+    },
+
+    hightlightName: function(e){
+      this.$el.toggleClass("warning");
+    },
+
+    deleteClicked: function(e){
+      e.stopPropagation();
+      alert("delete button was clicked");
+    }
+
   });
 
   List.Contacts = Marionette.CompositeView.extend({
