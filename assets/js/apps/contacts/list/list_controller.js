8669651e31906291c6092327a420ad14d3ae07e5
mobjohnson <mobjohnson@gmail.com>
2015-03-24 17:11:48 -0400

    Edit list_controller for search

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -10,12 +10,27 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
       var contactsListPanel = new List.Panel();
 
       $.when(fetchingContacts).done(function(contacts){
+        var filteredContacts = ContactManager.Entities.FilteredCollection({
+          collection: contacts,
+
+          filterFunction: function(filterCriterion){
+            var criterion = filterCriterion.toLowerCase();
+            return function(contact){
+              if(contact.get("firstName").toLowerCase().indexOf(criterion) !== -1
+                || contact.get("lastName").toLowerCase().indexOf(criterion) !== -1
+                || contact.get("phoneNumber").toLowerCase().indexOf(criterion) !== -1){
+                return contact;
+              }
+            };
+          }
+        });
+
         var contactsListView = new List.Contacts({
-          collection: contacts
+          collection: filteredContacts
         });
 
         contactsListPanel.on("contacts:filter", function(filterCriterion){
-          console.log("filter list with criterion", filterCriterion);
+          filteredContacts.filter(filterCriterion);
         });
 
         contactsListLayout.on("show", function(){
