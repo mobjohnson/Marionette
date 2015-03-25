137b6bce3358de3edac3340ce9bbb1ba740984e3
mobjohnson <mobjohnson@gmail.com>
2015-03-25 15:12:51 -0400

    Highlight active header menu item

--- assets/js/apps/header/list/list_controller.js
+++ assets/js/apps/header/list/list_controller.js
@@ -4,7 +4,20 @@ ContactManager.module("HeaderApp.List", function(List, ContactManager, Backbone,
       var links = ContactManager.request("header:entities");
       var headers = new List.Headers({collection: links});
 
+      headers.on("brand:clicked", function(){
+        ContactManager.trigger("contacts:list");
+      });
+
       ContactManager.headerRegion.show(headers);
+    },
+
+    setActiveHeader: function(headerUrl){
+      var links = ContactManager.request("header:entities");
+      var headerToSelect = links.find(function(header){
+        return header.get("url") === headerUrl;
+      });
+      headerToSelect.select();
+      links.trigger("reset");
     }
   };
 });
--- assets/js/apps/header/list/list_view.js
+++ assets/js/apps/header/list/list_view.js
@@ -1,13 +1,30 @@
 ContactManager.module("HeaderApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Header = Marionette.ItemView.extend({
     template: "#header-link",
-    tagName: "li"
+    tagName: "li",
+
+    onRender: function(){
+      if(this.model.selected){
+        // add class so Bootstrap will highlight
+        // the active entry in the navbar
+        this.$el.addClass("active");
+      };
+    }
   });
 
   List.Headers = Marionette.CompositeView.extend({
     template: "#header-template",
     className: "navbar navbar-inverse navbar-fixed-top",
     childView: List.Header,
-    childViewContainer: "ul"
+    childViewContainer: "ul",
+
+    events: {
+      "click a.brand": "brandClicked"
+    },
+
+    brandClicked: function(e){
+      e.preventDefault();
+      this.trigger("brand:clicked");
+    }
   });
 });
